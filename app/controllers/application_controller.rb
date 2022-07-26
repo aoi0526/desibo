19def0d4c354fd86951cbbcc157ce621cde96622
aoi0526 <gga9826@gmail.com>
2022-07-26 11:46:41 +0000

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :prefecture_id, :city, :occupation_genre_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :prefecture_id, :city])
   end
 end
